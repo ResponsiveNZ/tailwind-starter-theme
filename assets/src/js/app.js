1bea226578ab931a789ce687a2e01318893a4212
Cy Messenger <cy@responsiveweb.nz>
2021-09-08 15:43:31 +1200

    fixed Alpine integration. Add dropdown affect to mobile submenus

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -2,9 +2,14 @@ window.$ = window.jQuery = require('jquery');
 
 require('./main');
 
-import 'alpinejs';
-
 import 'glider-js';
+
+// import 'alpinejs';
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+
+Alpine.start()
 
 // import sal from 'sal.js';
 
