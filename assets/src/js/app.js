7b472d3ab9dab0c56338cbe7656676153772156b
Cy Messenger <cy@responsiveweb.nz>
2023-01-01 14:13:55 +1300

    Install Alpine's focus plugin. Tweak blog pages to work with Responsive\BlogExtensions plugin

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -6,9 +6,11 @@ import 'glider-js';
 
 // import 'alpinejs';
 import Alpine from 'alpinejs'
-
+import focus from '@alpinejs/focus'
+ 
 window.Alpine = Alpine
 
+Alpine.plugin(focus)
 Alpine.start()
 
 // import sal from 'sal.js';
