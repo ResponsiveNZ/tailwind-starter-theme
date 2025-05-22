5115a9d09b4a194d80f43f740b2265938329f82c
Cy Messenger <cy@responsiveweb.nz>
2025-05-22 22:12:09 +1200

    Complete migration from Static Pages to Tailor based solution

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,17 +10,19 @@ mix.setPublicPath('./')
     .js('assets/src/js/app.js', 'assets/dist/js')
     .postCss('assets/src/css/style.css', 'assets/dist/css', postCssPlugins)
     .browserSync({
-        proxy: 'https://octoberstarter.app',
-        host: 'octoberstarter.app',
+        proxy: 'https://octoberstarter.test',
+        host: 'octoberstarter.test',
         open: 'external',
         https: {
-            key: '/Users/responsive/.config/valet/Certificates/octoberstarter.app.key',
-            cert: '/Users/responsive/.config/valet/Certificates/octoberstarter.app.crt',
+            key: '/Users/responsive/.config/valet/Certificates/octoberstarter.test.key',
+            cert: '/Users/responsive/.config/valet/Certificates/octoberstarter.test.crt',
         },
         files: [
             'layouts/**/*.htm',
             'pages/**/*.htm',
             'partials/**/*.htm',
+            // 'assets/src/js/**/*.js',
+            // 'assets/src/css/**/*.css'
             'assets/dist/js/app.js',
             'assets/dist/css/style.css'
         ]
